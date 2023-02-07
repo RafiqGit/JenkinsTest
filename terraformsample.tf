a06c943e9a361f28a87926def45e15b26617bdfd
Rayaan <rahmanrafi@gmail.com>
2023-02-07 12:33:38 -0600

    Updated file with acc & sec key.

--- terraformsample.tf
+++ terraformsample.tf
@@ -2,13 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.16"
     }
   }
+  required_version = ">= 1.2.0"
 }
 
 # Configure the AWS Provider
 provider "aws" {
+  access_key = "AKIASTKEXLSFL6KNRZG4"
+  secret_key = "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"
   region = "us-east-1"
 }
 
